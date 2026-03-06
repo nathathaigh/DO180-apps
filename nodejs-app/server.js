4a30d2cec9d65e2a984a942772b3f34d44ca44d3
nathathaigh <75407985+nathathaigh@users.noreply.github.com>
2026-03-06 15:24:07 +0700

    Update server.js

--- nodejs-app/server.js
+++ nodejs-app/server.js
@@ -1,13 +1,12 @@
-var createError = require('http-error');
+var createError = require('http-errors');  // fix: http-error → http-errors
 
 var express = require('express');
-app = express();
+var app = express();                        // fix: added 'var' to avoid global variable
 
 app.get('/', function (req, res) {
-  res.send('Hello World from pod: ' + process.environment.HOSTNAME + '\n')
+  res.send('Hello World from pod: ' + process.env.HOSTNAME + '\n');  // fix: process.environment → process.env
 });
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
-
